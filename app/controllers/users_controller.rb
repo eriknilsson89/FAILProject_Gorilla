ab42b4e49936477f6e4719d24e9f52dcb816db92
Erik Nilsson <eriknilsson89@gmail.com>
2011-10-11 14:59:14 +0200

    Fixed working sign up and login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+    @title = "Registrering"
+  end  
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      sign_in @user
+      flash[:success] = "Registreringen lyckades!"
+      redirect_to @user
+    else
+      @title = "Registrering"
+      render 'new'
+    end
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+  
+  def show
+    @user = User.find(params[:id])
+    @title = @user.name
+  end
+
+end
